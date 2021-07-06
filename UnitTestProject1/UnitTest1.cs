2ed88372b3671e1575864734802e11b6c8e33523
Geralt Andreich <eric.cartman.1993@mail.ru>
2021-07-06 08:28:49 +0300

    Добавлены юнит тесты

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using modul;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0], -45.6);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0] * -1, 45.6);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[0, 0], 5.6);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            vvodZnach vz = new vvodZnach();
+            vz.simplexBol();
+            Assert.AreEqual(vz.table_result[2, 0], 0.4);
+        }
+    }
+}
+
--- modul/Program.cs
+++ modul/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mod
+namespace modul
 {
     public class Simplex
     {
