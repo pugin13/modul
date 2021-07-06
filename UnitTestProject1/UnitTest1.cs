1a556a71b09668fecb12a66ead7e0715bc64deca
Geralt Andreich <eric.cartman.1993@mail.ru>
2021-07-06 09:26:07 +0300

    Диаграммы

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -11,28 +11,28 @@ namespace UnitTestProject1
         {
             vvodZnach vz = new vvodZnach();
             vz.simplexBol();
-            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0], -45.6);
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0], -9,11111111111111);
         }
         [TestMethod]
         public void TestMethod2()
         {
             vvodZnach vz = new vvodZnach();
             vz.simplexBol();
-            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0] * -1, 45.6);
+            Assert.AreEqual(vz.table_result[vz.table_result.GetLength(0) - 1, 0] * -1, 9,11111111111111);
         }
         [TestMethod]
         public void TestMethod3()
         {
             vvodZnach vz = new vvodZnach();
             vz.simplexBol();
-            Assert.AreEqual(vz.table_result[0, 0], 5.6);
+            Assert.AreEqual(vz.table_result[0, 0], 0,666666666666667);
         }
         [TestMethod]
         public void TestMethod4()
         {
             vvodZnach vz = new vvodZnach();
             vz.simplexBol();
-            Assert.AreEqual(vz.table_result[2, 0], 0.4);
+            Assert.AreEqual(vz.table_result[2, 0], 0,888888888888889);
         }
     }
 }
